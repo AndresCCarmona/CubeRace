be8ccd2d48b8643bcfca43731683f9d2580e96c7
Andres Carmona <AndresCarmona@Mac-mini-de-Andres.local>
2018-10-02 00:13:03 -0500

    6 commit

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerCollision : MonoBehaviour {
 
     public CubeScript Movement;
+
 
 	private void OnCollisionEnter(Collision collisioninfo)
 	{
@@ -13,6 +15,7 @@ public class PlayerCollision : MonoBehaviour {
 
             Movement.enabled = false;
             Debug.Log("We hit a Obstacle");
+
 
 
         }
